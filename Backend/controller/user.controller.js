9ab40478aaea95e15757ed38a20407911ff838f4
abhishekumar09 <2003abhishek.09@gmail.com>
2024-08-19 05:28:52 +0530

    Backend JWT Token

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -35,15 +35,17 @@
 // logics define here 
 
 import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";                                              // libraray use for hash the password
 import createTokenAndSaveCookie from "../jwt/generateToken.js";
 
 export const signup = async (req, res) => {                                  // export use because hmein signup ka route create krenge aur usmein rkhenge post request means usmein information provide krenge
   try {                                                                       // try catch use krte hein ki error n aayee 
     const { name, email, password, confirmpassword } = req.body;              // ye sb chizein req.body se aa rhi hein (postman)
+       // Validate password and confirmation password
     if (password !== confirmpassword) {
       return res.status(400).json({ message: "Passwords do not match" });      // 400 use for showing data invalid
     }
+        // Check if the email already exists
     const user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ message: "Email already exists" });
@@ -56,6 +58,7 @@ export const signup = async (req, res) => {                                  // 
       email,
       password: hashedPassword,
     });
+        // Save the new user in the database
     await newUser.save();                                                     // .save use for saving the user data kyunki use dubara signup n krna pde
     if (newUser) {
       createTokenAndSaveCookie(newUser._id, res);
@@ -76,13 +79,15 @@ export const signup = async (req, res) => {                                  // 
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
+      // Check if the user exists in the database
   try {
-    const user = await User.findOne({ email });
-    const isMatch = await bcrypt.compare(password, user.password);
+    const user = await User.findOne({ email });                              // await use for serisleise result avoid from ulta pulta result
+    const isMatch = await bcrypt.compare(password, user.password);           // it use for matching the password       // Compare the password with the hashed password in the database
     if (!user || !isMatch) {
       return res.status(404).json({ message: "invalid User or Password" });
     }
-    createTokenAndSaveCookie(user._id, res);
+        // If successful, create token and send response
+    createTokenAndSaveCookie(user._id, res);                                // jb login hojaye uske pass toke hona chaiye
     res.status(201).json({
       message: "User logged in successfully",
       user: {
@@ -106,3 +111,16 @@ export const logout = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+
+
+
+
+
+
+
+
+// async - use for function perform krne k liye  aur jitni bhi promises hei use handle krane ke liye 
+// await - jb bhi hm data bhj rhe hote hein use aane mein time lgta hei isliye yeh dono use krte hein await use for waiting
+
+// jhan jhan databsae aa rha hei vhan await ko use krein
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signup } from "../controller/user.controller.js";
-
+import { signup, login, logout } from "../controller/user.controller.js";
 
 const router = express.Router();
 
-router.post("/signup",signup);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
