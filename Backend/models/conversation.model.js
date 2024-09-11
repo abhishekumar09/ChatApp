5075850afff56b659e99a1da73cdb10c77f572b3
abhishekumar09 <2003abhishek.09@gmail.com>
2024-09-11 09:11:02 +0530

    model

---
+++ Backend/models/conversation.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import Message from "./message.model.js";
+const conversationSchema = new mongoose.Schema(
+  {
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+      },
+    ],
+    messages: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Message,
+        default: [],
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Conversation = mongoose.model("conversation", conversationSchema);
+export default Conversation;
